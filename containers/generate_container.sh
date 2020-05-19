914f1f7ebe9fa45acb3d0a9f01e662bfa43ee3d6
Yaroslav Halchenko <debian@onerussian.com>
2020-05-19 12:39:28 -0400

    neurodocker invocation to produce container (elderly jessie) to build website
    At some point after jessie I found that the menu disappears when built
    with newer pelican - never troubleshooted to have it fixed. So for now
    will just use this container (list of packages was verified in
    interactive docker session)

---
+++ containers/generate_container.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -eu
+
+generate() {
+	# more details might come on https://github.com/ReproNim/neurodocker/issues/330
+	[ "$1" == singularity ] && add_entry=' "$@"' || add_entry=''
+	#neurodocker generate "$1" \
+	ndversion=0.7.0
+	#ndversion=master
+	docker run --rm repronim/neurodocker:$ndversion generate "$1" \
+		--base=neurodebian:jessie \
+		--ndfreeze date=20200503 \
+		--pkg-manager=apt \
+		--install vim wget strace time ncdu gnupg curl procps pigz less tree \
+				  python-pelican linkchecker git git-annex-standalone make \
+		--user=nobody
+}
+
+#generate docker > Dockerfile
+generate singularity > Singularity
+
