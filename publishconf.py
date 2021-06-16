92c66edc9c4aa0438e289c02611af67ed4d708a2
Yaroslav Halchenko <debian@onerussian.com>
2021-06-16 15:01:18 -0400

    Make https to be the official one and remove shebang

--- publishconf.py
+++ publishconf.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
@@ -10,7 +9,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://centerforopenneuroscience.org'
+SITEURL = 'https://centerforopenneuroscience.org'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
